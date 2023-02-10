eb18797633253cd4b29d9bd2502f7cd6db97d587
jkap86 <jkap86@gmail.com>
2023-02-10 07:22:02 -0500

    -

--- server/server.js
+++ server/server.js
@@ -1,42 +1,52 @@
 'use strict'
-const express = require('express');
-const cors = require('cors');
-const compression = require('compression');
-const path = require('path');
-const cluster = require('cluster');
 
-const app = express();
+const throng = require('throng');
+const WORKERS = process.env.WEB_CONCURRENCY || 1;
 
-app.use(compression())
-app.use(cors());
-app.use(express.json());
-app.use(express.static(path.resolve(__dirname, '../client/build')));
-
-
-const db = require("./app/models");
-db.sequelize.sync()
-    .then(() => {
-        console.log("Synced db.");
-    })
-    .catch((err) => {
-        console.log("Failed to sync db: " + err.message);
-    })
-
-
-require("./app/routes/sync.routes")(app);
-require("./app/routes/home.routes")(app);
-require("./app/routes/user.routes")(app);
-require("./app/routes/league.routes")(app);
-require("./app/routes/trade.routes")(app);
-require("./app/routes/playoffs.routes")(app);
-
-app.get('*', async (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-})
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
+throng({
+    worker: start,
+    count: WORKERS
 });
 
+function start() {
+    const express = require('express');
+    const cors = require('cors');
+    const compression = require('compression');
+    const path = require('path');
+    const cluster = require('cluster');
 
+    const app = express();
+
+    app.use(compression())
+    app.use(cors());
+    app.use(express.json());
+    app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+
+    const db = require("./app/models");
+    db.sequelize.sync()
+        .then(() => {
+            console.log("Synced db.");
+        })
+        .catch((err) => {
+            console.log("Failed to sync db: " + err.message);
+        })
+
+
+    require("./app/routes/sync.routes")(app);
+    require("./app/routes/home.routes")(app);
+    require("./app/routes/user.routes")(app);
+    require("./app/routes/league.routes")(app);
+    require("./app/routes/trade.routes")(app);
+    require("./app/routes/playoffs.routes")(app);
+
+    app.get('*', async (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    })
+
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}.`);
+    });
+
+}
